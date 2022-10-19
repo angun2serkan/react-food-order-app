cbf31f035511ffc58c122a61495c2246264625f9
Serkan Angün <angun.serkan2@gmail.com>
2022-10-19 21:05:31 +0300

    "Header " component is added

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Header.module.css";
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img
+          src="https://raw.githubusercontent.com/academind/react-complete-guide-code/11-practice-food-order-app/extra-files/meals.jpg"
+          alt="a table full of delicious food"
+        ></img>
+        {/* if we have got image in our folders we need to import it  then give it to the src attribute as an object */}
+      </div>
+    </>
+  );
+};
+
+export default Header;
