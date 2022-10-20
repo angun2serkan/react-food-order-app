fbe51941ad261b3d4907173a577458a8ef3cbb4d
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-10-20 08:35:56 +0300

    MealItem component is added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from '../UI/Card.js';
+import MealItem from "./MealItem/MealItem.js";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -29,12 +31,14 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>);
 
   return (
     <>
       <section className={classes.meals}>
+        <Card>
         <ul>{mealsList}</ul>
+        </Card>
       </section>
     </>
   );
