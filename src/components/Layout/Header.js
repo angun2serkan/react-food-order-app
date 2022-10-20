df26eac8cbacddd856267a741ea74e0ee5b4d906
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-10-20 14:17:32 +0300

    Cart open close functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import HeaderCartutton from "./HeaderCartButton.js";
+import HeaderCartButton from "./HeaderCartButton.js";
 import classes from "./Header.module.css";
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartutton></HeaderCartutton>
+        <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img
