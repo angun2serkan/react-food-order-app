cf94fdf685d644406c896fb4a355b8b8b1a6ba47
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-10-20 11:32:08 +0300

    Modal is added by portal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+    return <div className={classes.backdrop}></div>;
+};
+
+
+const ModalOverlay = (props) => {
+    return (<div className={classes.modal}>
+        <div className={classes.content}>{props.children}</div>
+    </div>)
+}
+
+
+const portalElement = document.getElementById('overlays');
+
+
+const Modal = (props) => {
+
+    return (<>
+        {ReactDOM.createPortal(<Backdrop></Backdrop>, portalElement)}
+        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+        </>);
+};
+
+export default Modal;
+
+
+// creating a portal steps:
+// 1) create a html element in index.html (id='overlays')
+// 2) create components that will put into portal
+// 3) create portal put component and describe the location components will be sent (id='overlays')
