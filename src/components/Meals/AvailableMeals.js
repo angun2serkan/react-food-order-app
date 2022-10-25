61eee17a4eec37b7176ab5b0ef50d0c38c12829c
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-10-25 15:13:40 +0300

    Handling Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -33,11 +33,18 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
 
   const[meals, setMeals] = useState([]);
+  const[isLoading, setIsLoading] = useState(true);
+  const[httpError, setHttpError] = useState(false);
 
 
   useEffect(() => {
     const fetchMeals = async() => {
+
       const response = await fetch('https://react-http-9b1de-default-rtdb.firebaseio.com/meals.json');
+
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -52,9 +59,31 @@ const AvailableMeals = () => {
     }
 
     setMeals(loadedMeals);
+    setIsLoading(false);
   };
-    fetchMeals();
+
+  fetchMeals().catch((error) => {
+    setIsLoading(false);
+    setHttpError(error.message);
+  });
   }, []);
+
+  if(isLoading){
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if(httpError){
+    return (
+      <section className={classes.mealsError}>
+      <p>{httpError}</p>
+    </section>
+    )
+  }
+
   const mealsList = meals.map((meal) => <MealItem 
   key={meal.id} 
   id={meal.id}
